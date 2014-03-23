82c03db49a54326c5d18d5f111639e98164b9a87
Chris <git.simpkins@gmail.com>
2014-03-23 09:52:01 -0400

    limited terminal open from sidebar to the active window

--- GlueSidebarOpener.py
+++ GlueSidebarOpener.py
@@ -13,39 +13,39 @@ else:
 
 class GlueSidebarOpenerCommand(sublime_plugin.WindowCommand):
     def run(self, paths = []):
-        for path in paths:
-            if os.path.exists(path) and os.path.isfile(path) and path.endswith('.glue'):
-                self.erase_existing_glue_file(path) # clear the terminal text in .glue files
+        path = paths[0] # only use the first of the paths in the passed argument (prevents multiple terminals from opening)
+        if os.path.exists(path) and os.path.isfile(path) and path.endswith('.glue'):
+            self.erase_existing_glue_file(path) # clear the terminal text in .glue files
+            self.open_the_file(path)
+            self.window.active_view().run_command('glue')
+        elif os.path.exists(path) and os.path.isfile(path):
+            file_path = os.path.join(os.path.dirname(path),'terminal.glue')
+            FileWriter(file_path).write_utf8(" ")
+            self.open_the_file(file_path)
+            self.window.active_view().run_command('glue')
+        elif os.path.exists(path) and os.path.isdir(path):
+            # create a .glue terminal file in the directory if it does not exist
+            glue_filenames = [name for name in os.listdir(path) if name.endswith('.glue')]
+            if len(glue_filenames) > 0:
+                path = glue_filenames[0]
+                if path.endswith('.glue'):
+                    self.erase_existing_glue_file(path) # clear the terminal text in .glue files
                 self.open_the_file(path)
                 self.window.active_view().run_command('glue')
-            elif os.path.exists(path) and os.path.isfile(path):
-                file_path = os.path.join(os.path.dirname(path),'terminal.glue')
-                FileWriter(file_path).write_utf8(" ")
-                self.open_the_file(file_path)
-                self.window.active_view().run_command('glue')
-            elif os.path.exists(path) and os.path.isdir(path):
-                # create a .glue terminal file in the directory if it does not exist
-                glue_filenames = [name for name in os.listdir(path) if name.endswith('.glue')]
-                if len(glue_filenames) > 0:
-                    path = glue_filenames[0]
-                    if path.endswith('.glue'):
-                        self.erase_existing_glue_file(path) # clear the terminal text in .glue files
-                    self.open_the_file(path)
-                    self.window.active_view().run_command('glue')
-                else:
-                    # write a terminal.glue file in the selected directory
-                    FileWriter('terminal.glue').write_utf8(" ")
-                    if os.path.exists('terminal.glue'):
-                        self.open_the_file('terminal.glue')
-                        self.window.active_view().run_command('glue')
             else:
-                sublime.error_message("Glue Plugin Error : Glue was not able to launch the terminal from the selected path.  Try opening the file in your editor and then launching Glue from the Command Palette.")
+                # write a terminal.glue file in the selected directory
+                FileWriter('terminal.glue').write_utf8(" ")
+                if os.path.exists('terminal.glue'):
+                    self.open_the_file('terminal.glue')
+                    self.window.active_view().run_command('glue')
+        else:
+            sublime.error_message("Glue Plugin Error : Glue was not able to launch the terminal from the selected path.  Try opening the file in your editor and then launching Glue from the Command Palette.")
 
     def open_the_file(self, path):
-        for window in sublime.windows():
-            view = window.open_file(path)
-            # window.set_view_index(view, view_index[0], view_index[1])
-            sublime.active_window().focus_view(view)
+        window = sublime.active_window()
+        view = window.open_file(path)
+        # window.set_view_index(view, view_index[0], view_index[1])
+        sublime.active_window().focus_view(view)
 
     def go_to_end(self):
         line, row = self.window.active_view().rowcol(self.window.active_view().size())
