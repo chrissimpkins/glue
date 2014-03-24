627b6658ef1e1d3116fbd611a98d5ee2c2688386
Chris <git.simpkins@gmail.com>
2014-03-23 23:19:33 -0400

    added glue commands module with file open and wildcard file open commands

---
+++ GlueCmds.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import sublime
+import sublime_plugin
+import os
+from glob import glob
+
+class GlueFileOpenerCommand(sublime_plugin.WindowCommand):
+    def run(self, paths = [], current_dir="", file_list=[]):
+        if len(file_list) > 1:
+            for file in file_list:
+                abs_filepath = os.path.join(current_dir, file)
+                self.file_opener(abs_filepath)
+        else:
+            abs_filepath = os.path.join(current_dir, file_list[0])
+            self.file_opener(abs_filepath)
+
+    def file_opener(self, file_path):
+        if os.path.exists(file_path) and os.path.isfile(file_path):
+            self.window.open_file(file_path)
+
+class GlueFileWildcardOpenerCommand(sublime_plugin.WindowCommand):
+    def run(self, paths = [], current_dir="", match_pattern=""):
+        if len(current_dir) > 0 and len(match_pattern) > 0:
+            os.chdir(current_dir)
+            file_list = glob(match_pattern)
+            if len(file_list) > 0:
+                for file in file_list:
+                    abs_filepath = os.path.join(current_dir, file)
+                    self.file_opener(abs_filepath)
+
+    def file_opener(self, file_path):
+            if os.path.exists(file_path) and os.path.isfile(file_path):
+                self.window.open_file(file_path)
