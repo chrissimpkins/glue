4e5e5f3c69a45fac440b2e858e61669d2e246570
Chris <git.simpkins@gmail.com>
2014-04-21 09:10:41 -0400

    refactored variable name, changed spacing on if block

--- GlueCmds.py
+++ GlueCmds.py
@@ -10,10 +10,10 @@ from glob import glob
 # [ GlueFileOpenerCommand class ] - executed from glue open command
 #------------------------------------------------------------------------------
 class GlueFileOpenerCommand(sublime_plugin.WindowCommand):
-    def run(self, paths = [], current_dir="", file_list=[]):
+    def run(self, paths=[], current_dir="", file_list=[]):
         if len(file_list) > 1:
-            for file in file_list:
-                abs_filepath = os.path.join(current_dir, file)
+            for the_file in file_list:
+                abs_filepath = os.path.join(current_dir, the_file)
                 self.file_opener(abs_filepath)
         else:
             abs_filepath = os.path.join(current_dir, file_list[0])
@@ -27,15 +27,15 @@ class GlueFileOpenerCommand(sublime_plugin.WindowCommand):
 # [ GlueFileWildcardOpenerCommand class ] - executed from glue wco command
 #------------------------------------------------------------------------------
 class GlueFileWildcardOpenerCommand(sublime_plugin.WindowCommand):
-    def run(self, paths = [], current_dir="", match_pattern=""):
+    def run(self, paths=[], current_dir="", match_pattern=""):
         if len(current_dir) > 0 and len(match_pattern) > 0:
             os.chdir(current_dir)
             file_list = glob(match_pattern)
             if len(file_list) > 0:
-                for file in file_list:
-                    abs_filepath = os.path.join(current_dir, file)
+                for the_file in file_list:
+                    abs_filepath = os.path.join(current_dir, the_file)
                     self.file_opener(abs_filepath)
 
     def file_opener(self, file_path):
-            if os.path.exists(file_path) and os.path.isfile(file_path):
-                self.window.open_file(file_path)
+        if os.path.exists(file_path) and os.path.isfile(file_path):
+            self.window.open_file(file_path)
