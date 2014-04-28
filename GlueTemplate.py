1f59f6621054e787cb5fceefd2f6e0640a9f056f
Chris <git.simpkins@gmail.com>
2014-04-28 01:24:52 -0400

    new GlueTemplate module

---
+++ GlueTemplate.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from GlueInk import Template, Renderer
+from GlueIO import FileReader, FileWriter
+
+class TemplateCommander(object):
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+
+    def _get_local_template(self):
+        pass
+
+    def _get_remote_template(self):
+        pass
+
+    def _write_template(self):
+        pass
